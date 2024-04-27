60de31c5db70c25e8e990c70dcfbdac4e84216c4
Gözde <altuntasgozde@gmail.com>
2024-04-27 14:53:52 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+
+
+  return (
+    <>
+      <div class="max-w-sm space-y-3">
+        <input type="text" class="py-3 px-4 block w-full border border-gray-700 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="This is placeholder" />
+      </div>
+    </>
+  )
+}
+
+export default App
